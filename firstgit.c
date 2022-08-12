fbaa070c0147f960892b9d7bcb9b5614d4a16168
muskaangabra <110661383+muskaangabra@users.noreply.github.com>
2022-08-12 21:52:41 +0530

    Add files via upload

---
+++ firstgit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+  int num1, num2, diff;
+  
+  printf("Enter the first number: ");
+  scanf("%d", &num1);
+  printf("Enter the second number: ");
+  scanf("%d", &num2);
+  
+  diff = num1 - num2;
+  printf("Difference of %d and %d is: %d", num1, num2, diff);
+  return 0;
+}
